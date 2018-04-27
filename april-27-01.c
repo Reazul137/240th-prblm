641f5b8c426fffa661e49f51a7797bf73388670f
Reazul Islam <reazulislam137@gmail.com>
2018-04-28 00:37:10 +0600

    first commit

---
+++ april-27-01.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n, i;
+    long sum=0;
+    long int t=1;
+
+    printf("Input the numbers of terms : ");
+    scanf("%d",&n);
+
+    for (i=1; i<=n; i++)
+    {
+        printf("%ld  ",t);
+        if(i<n)
+        {
+            printf("+  ");
+        }
+        sum= sum + t;
+        t = (t * 10) + 1;
+    }
+    printf("\n The sum is : %ld\n",sum);
+    return 0;
+}
